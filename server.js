fbb2876c3c6d9b89a9031f1b4e487de4ce8b63e0
Anuj Rai <anujrai1701@gmail.com>
2025-02-19 18:03:13 +0530

    added validation message

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.send('StreetBites API is running');
+});
+
 // Setting up routes for authentication, recipes, and interactions
 app.use('/api/auth', require('./src/routes/auth'))
 app.use('/api/recipes', require('./src/routes/recipes'))
